8266c8af71ad07ea0bf49fd508d713a0f916da49
laouyassine <yassinelaouane@outlook.fr>
2023-07-24 14:42:50 +0200

    POST et GET avec response

--- src/main/java/fr/maboite/correction/rest/controller/MonDeuxiemeRestController.java
+++ src/main/java/fr/maboite/correction/rest/controller/MonDeuxiemeRestController.java
@@ -1,16 +1,68 @@
 package fr.maboite.correction.rest.controller;
 
+import fr.maboite.correction.rest.pojo.ArticlePojo;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/articles")
+@Produces(MediaType.APPLICATION_JSON)
 public class MonDeuxiemeRestController {
-	
-	@GET
-	public String maDeuxiemeeMethode() { // URL que traite cette méthode : 
-		// URL application + URL de la classe + URL de la méthode
-		System.out.println("maDeuxièmeMethode est appelée");
-		return "ok";
-	}
 
+    @GET
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public ArticlePojo getArticles(@PathParam("id") Integer id) {
+        System.out.println("La méthode getArticles est appelée avec l'id: " + id);
+        ArticlePojo article = new ArticlePojo();
+        article.setId(id);
+        article.setName("Article " + id);
+        article.setDescription("L'article " + id + " est valide");
+        return article;
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getAllArticles() {
+        ArticlePojo article = new ArticlePojo();
+        article.setId(1);
+        article.setName("Article 1");
+        article.setDescription("Ceci est l'article 1");
+
+        return article.toString();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public ArticlePojo postArticles(ArticlePojo article) {
+        if (article.getId() > 0) {
+            return article;
+        }
+        article.setId(5);
+        return article;
+    }
+    
+    @GET
+    @Path("/Response/{id}")
+    public Response getResponseArticles(@PathParam("id") Integer id) {
+        if (id < 0) {
+        	return Response.status(Response.Status.NOT_FOUND)
+        			.header("mon header","c'est négatif")
+        			.build();
+        }
+        ArticlePojo article = new ArticlePojo();
+
+        article.setId(id);
+        article.setName("Article " + id);
+        article.setDescription("Cet Id" + id + " est valide");
+        return Response.ok(article).build();
+        		
+    }
+ 
 }
